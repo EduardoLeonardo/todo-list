4ddca761331962d4cf5c023c1c9f9aca9b9d5c60
Eduardo Leonardo <eduardoleonardo1@hotmail.com>
2020-11-25 19:48:18 -0300

    user component

---
+++ src/components/User.vue
@@ -0,0 +1,35 @@
+<template>
+    <div> 
+        <form>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control"  v-model="email" >
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" v-model="password" id="exampleInputPassword1">
+            </div>
+            <button type="submit" class="btn btn-primary" v-on:click="show()">Submit</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: "",
+            password: ""          
+        }
+    
+    },
+    methods: {
+        show: function () { console.log(this.email +'-' +this.password)}
+    }
+
+}
+</script>
+
+<style>
+
+</style>
